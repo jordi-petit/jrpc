40a075c6008836881ddd04b157a4345b94b1d3a7
Jordi Petit <jordi.petit@gmail.com>
2024-11-03 08:50:50 +0100

    Improve exception handling in the client

--- api.py
+++ api.py
@@ -3,21 +3,22 @@ import requests
 import json
 from typing import Any, TypedDict
 
+def _execute(name: str, arg: Any) -> Any:
+    response = requests.post('http://localhost:8000/jrpc', json={"name": name, "arg": arg})
+    result = response.json()
+    if result['error']:
+        raise Exception(result['error'])
+    # estaria bé verificar aquí que el tipus retornat compleix amb el promès (l'API ja ho fa però així el client també en tindria la seguretat)
+    return result['result']
 
 
 def uppercase(arg: str) -> str:
     '''Converts a string to uppercase'''
 
-    response = requests.post('http://localhost:8000/jrpc', json={"name": "uppercase", "arg": arg})
-    result = response.json()
-    # estaria bé verificar que el tipus retornat compleix amb el promès (l'API ja ho fa però així el client també en tindria la seguretat)
-    return result
+    return _execute('uppercase', arg)
 
 
 def division(arg: {'a': float, 'b': float}) -> float:
     '''Divides two numbers'''
 
-    response = requests.post('http://localhost:8000/jrpc', json={"name": "division", "arg": arg})
-    result = response.json()
-    # estaria bé verificar que el tipus retornat compleix amb el promès (l'API ja ho fa però així el client també en tindria la seguretat)
-    return result
+    return _execute('division', arg)
--- jrpc.tsx
+++ jrpc.tsx
@@ -117,6 +117,13 @@ import requests
 import json
 from typing import Any, TypedDict
 
+def _execute(name: str, arg: Any) -> Any:
+    response = requests.post('http://localhost:8000/jrpc', json={"name": name, "arg": arg})
+    result = response.json()
+    if result['error']:
+        raise Exception(result['error'])
+    # estaria bé verificar aquí que el tipus retornat compleix amb el promès (l'API ja ho fa però així el client també en tindria la seguretat)
+    return result['result']
 `
 
         for (const [name, { input, output, summary }] of this.endpoints) {
@@ -125,10 +132,7 @@ from typing import Any, TypedDict
 def ${name}(arg: ${typify(input)}) -> ${typify(output)}:
     '''${summary}'''
 
-    response = requests.post('http://localhost:8000/jrpc', json={"name": "${name}", "arg": arg})
-    result = response.json()
-    # estaria bé verificar que el tipus retornat compleix amb el promès (l'API ja ho fa però així el client també en tindria la seguretat)
-    return result
+    return _execute('${name}', arg)
 `
         }
         return python
